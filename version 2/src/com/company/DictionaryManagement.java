53228233e4b0863ba1ebfd8bc2b5e33e391e87f4
nguyentiendat1531999@gmail.com <nguyentiendat1531999@gmail.com>
2018-10-01 16:54:42 +0700

    version 2

---
+++ version 2/src/com/company/DictionaryManagement.java
@@ -0,0 +1,68 @@
+package com.company;
+
+import sun.applet.Main;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DictionaryManagement {
+
+    public static void insertFromCommandline() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of new word:");
+        int n = sc.nextInt();
+        sc.nextLine();
+        System.out.println();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Input english word " + i + ": ");
+            String english = sc.nextLine();
+            System.out.print("Input vietnamese meaning " + i + ": ");
+            String vietnamese = sc.nextLine();
+            System.out.println();
+            Word word = new Word(english, vietnamese);
+            Dictionary.myDic.add(new Word(english, vietnamese));
+        }
+        sc.close();
+    }
+    public  static void insertFromFile()
+    {
+
+        try {
+            FileReader fileReader = new FileReader("src\\com\\company\\dictionaries.txt");
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line = null;
+            while ((line = reader.readLine())!=null)
+            {
+                String [] read_word = line.split("\t");
+                String english = read_word[0];
+                String vietnamese = read_word[1];
+                Dictionary.myDic.add(new Word(english,vietnamese));
+            }
+            reader.close();
+        } catch (FileNotFoundException ex)
+        {
+            System.out.println("File not found!");
+        } catch (IOException ex)
+        {
+            System.out.println("Unexpected error reading file");
+        }
+    }
+    public static String dictionaryLookup(String english_word)
+    {
+        String ans = "word not found";
+        for (Word word: Dictionary.myDic)
+        {
+            if (word.getWord_target().equals(english_word))
+            {
+                return word.getWord_explain();
+            }
+        }
+        return ans;
+    }
+}
+
+
